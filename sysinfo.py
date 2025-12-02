59ce8b036ea05f53e6e5b7bd7abdbf6143218079
Arham <snehazahan@yahoo.com>
2025-12-02 17:13:44 -0500

    Create sysinfo.py

---
+++ sysinfo.py
@@ -0,0 +1,6 @@
+def run():
+    print("SolarOS System Information")
+    print("--------------------------")
+    print("Version: 3.0 X (2009)")
+    print("Memory: 128MB Virtual")
+    print("Kernel: SolarKernel 2.7-r5")
